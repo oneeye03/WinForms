08c85e205734557c8fbf1be388fec02c515d629e
one-eye <71996935+oneeye03@users.noreply.github.com>
2021-02-01 07:23:45 +0800

    Add files via upload

---
+++ WinformProject/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace WinformProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.emailTbox = new System.Windows.Forms.TextBox();
+            this.passTbox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.loginBtn = new System.Windows.Forms.Button();
+            this.logRcrdsTbox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Email";
+            // 
+            // emailTbox
+            // 
+            this.emailTbox.Location = new System.Drawing.Point(63, 41);
+            this.emailTbox.Name = "emailTbox";
+            this.emailTbox.Size = new System.Drawing.Size(200, 23);
+            this.emailTbox.TabIndex = 1;
+            // 
+            // passTbox
+            // 
+            this.passTbox.Location = new System.Drawing.Point(359, 41);
+            this.passTbox.Name = "passTbox";
+            this.passTbox.Size = new System.Drawing.Size(200, 23);
+            this.passTbox.TabIndex = 2;
+            this.passTbox.UseSystemPasswordChar = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(296, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password";
+            // 
+            // loginBtn
+            // 
+            this.loginBtn.Location = new System.Drawing.Point(598, 41);
+            this.loginBtn.Name = "loginBtn";
+            this.loginBtn.Size = new System.Drawing.Size(75, 23);
+            this.loginBtn.TabIndex = 3;
+            this.loginBtn.Text = "Login";
+            this.loginBtn.UseVisualStyleBackColor = true;
+            this.loginBtn.Click += new System.EventHandler(this.loginBtn_Click);
+            // 
+            // logRcrdsTbox
+            // 
+            this.logRcrdsTbox.Location = new System.Drawing.Point(21, 92);
+            this.logRcrdsTbox.Multiline = true;
+            this.logRcrdsTbox.Name = "logRcrdsTbox";
+            this.logRcrdsTbox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.logRcrdsTbox.Size = new System.Drawing.Size(652, 274);
+            this.logRcrdsTbox.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 386);
+            this.Controls.Add(this.logRcrdsTbox);
+            this.Controls.Add(this.loginBtn);
+            this.Controls.Add(this.passTbox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.emailTbox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox emailTbox;
+        private System.Windows.Forms.TextBox passTbox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button loginBtn;
+        private System.Windows.Forms.TextBox logRcrdsTbox;
+    }
+}
+
---
+++ WinformProject/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace WinformProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void loginBtn_Click(object sender, EventArgs e)
+        {
+            //logRcrdsTbox.Text = await ExecuteAsync(emailTbox.Text);
+
+            /*var userName = emailTbox.Text;
+            var passwd = passTbox.Text;
+            var url = "https://github.com/settings/security-log";
+
+            using var client = new HttpClient();
+
+            var authToken = Encoding.ASCII.GetBytes($"{userName}:{passwd}");
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic",
+                    Convert.ToBase64String(authToken));
+
+            HttpResponseMessage response = await client.GetAsync(url);
+            HttpContent content = response.Content;
+            var result = await content.ReadAsStringAsync();
+
+            //var content = await result.Content.ReadAsStringAsync();
+
+            logRcrdsTbox.Text = result.ToString();*/
+
+            /*var t = Task.Run(() => GetURI(new Uri("https://github.com/settings/security-log")));
+            t.Wait();
+
+            logRcrdsTbox.Text = t.Result;*/
+
+            /*var client = new HttpClient();
+            client.BaseAddress = new Uri("https://github.com");
+            var request = new HttpRequestMessage(HttpMethod.Post, "/settings/security-log");
+
+            var byteArray = Encoding.ASCII.GetBytes($"{emailTbox.Text}:{ passTbox.Text}");
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+
+            var formData = new List<KeyValuePair<string, string>>();
+            formData.Add(new KeyValuePair<string, string>("grant_type", "password"));
+            formData.Add(new KeyValuePair<string, string>("username", emailTbox.Text));
+            formData.Add(new KeyValuePair<string, string>("password", passTbox.Text));
+            formData.Add(new KeyValuePair<string, string>("scope", "all"));
+
+            request.Content = new FormUrlEncodedContent(formData);
+            var response = await client.SendAsync(request);
+
+            logRcrdsTbox.Text = response.ToString(); */
+
+            var httpClient = new HttpClient
+            {
+                BaseAddress = new Uri("https://api.github.com/graphql")
+            };
+
+            httpClient.DefaultRequestHeaders.Add("User-Agent", "WinForms");
+
+            string basicValue = Convert.ToBase64String(Encoding.UTF8.GetBytes($"{emailTbox.Text}:0b06823e7bb27cbf06cd4e0c52926d7ce9f4df57"));
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", basicValue);
+
+            var queryObject = new
+            {
+                query = @"query { 
+                viewer{
+                    login
+                    
+                }
+}",/*logs{ 
+                        event
+                        time
+                    }*/
+                variables = new { }
+            };
+
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Post,
+                Content = new StringContent(JsonConvert.SerializeObject(queryObject), Encoding.UTF8, "application/json")
+            };
+
+            dynamic responseObj;
+
+            using (var response = await httpClient.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+
+                var responseString = await response.Content.ReadAsStringAsync();
+                responseObj = JsonConvert.DeserializeObject<dynamic>(responseString);
+
+
+                logRcrdsTbox.Text = responseObj.ToString();
+            }
+
+            //Console.WriteLine(responseObj.data.viewer.login);
+
+        }
+
+
+
+        /*static async Task<string> GetURI(Uri u)
+        {
+            var response = string.Empty;
+            using (var client = new HttpClient())
+            {
+                var data = new StringContent(json, Encoding.UTF8, "application/json");
+                HttpResponseMessage result = await client.GetAsync(u);
+                if (result.IsSuccessStatusCode)
+                {
+                    response = await result.Content.PostAsync(u);
+                }
+            }
+            return response;
+        }*/
+
+        /*public static async Task<String> ExecuteAsync(String username)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://api.github.com");
+            var token = "0b06823e7bb27cbf06cd4e0c52926d7ce9f4df57";
+
+            client.DefaultRequestHeaders.UserAgent.Add(new System.Net.Http.Headers.ProductInfoHeaderValue("AppName", "1.0"));
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Token", token);
+
+            var response = await client.PostAsync("/users" + username + "/events");
+
+            return response.ToString();
+        }*/
+
+    }
+}
